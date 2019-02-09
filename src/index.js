5f11b51ada8b78060a62bf70198b18b718601d8a
divo93 <divgarg93@gmail.com>
2019-02-09 13:30:34 +0530

    getting differnt files data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import './style.css'
+import print from './print.js'
 
 function component () {
 	let element = document.createElement("div");
+	let btn = document.createElement("button");
 	element.innerHTML = ("Hi Webpack");
 	element.classList.add('hello')
+
+	btn.innerHTML = "Click";
+	btn.onclick = print;
+
+	element.appendChild(btn)
 	return element;
 }
 
