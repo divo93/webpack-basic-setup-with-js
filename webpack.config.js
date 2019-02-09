1002e5e7686ec2c66c91bfb16c2b8922f6963e47
Divya Gupta <divgarg93@gmail.com>
2019-02-09 12:32:56 +0530

    added Assets: CSS

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
+
 };
